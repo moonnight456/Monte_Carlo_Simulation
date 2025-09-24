5c47a291768717704ca3ae09ff1d36f701142ee2
Lunamoon456 <csj646578@gmail.com>
2025-09-24 13:55:42 +0900

    Add files via upload

---
+++ PI.py
@@ -0,0 +1,26 @@
+from random import randint
+
+r=1000
+circle_scale=(r**2)/4
+square_scale=r**2
+
+dot=1000000
+sim=10
+pi_sum=0
+
+print("Simulation Starts")
+
+for n in range(1,sim+1):
+    inn=0
+    for i in range(dot):
+        x=randint(1,r)
+        y=randint(1,r)
+        length=(x**2+y**2)**(1/2)
+        if length<=r:
+            inn+=1
+        #print(f"loop : {i}")
+    pi=(inn*square_scale)/(circle_scale*dot)
+    print(f"test{n} : {pi}")
+    pi_sum+=pi
+pi_sum/=sim
+print(f"PI : {pi_sum}")
